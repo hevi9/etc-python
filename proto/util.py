863142eb3e1be9867a82689d1e16de3466ce47ba
Petri Heinilä <hevi@lut.fi>
2012-11-08 17:31:16 +0200

    created reload

--- proto/util.py
+++ proto/util.py
@@ -36,6 +36,9 @@ Usage::
   def func():
     a = "value"
     log.debug(f"{a} and {b}")
+    
+CUI
+===
 """
 
 ##############################################################################
@@ -43,6 +46,9 @@ Usage::
 
 import collections # http://docs.python.org/release/3.2.3/library/collections.html
 import inspect # http://docs.python.org/release/3.2.3/library/inspect.html
+import argparse # http://docs.python.org/py3k/library/argparse.html
+import logging # http://docs.python.org/py3k/library/logging.html
+log = logging.getLogger(__name__)
 __all__ = list()
 
 ##############################################################################
@@ -82,3 +88,41 @@ def f(s):
   combi.update(caller.f_locals)
   return s.format(**combi)
 __all__.append("f")
+
+##############################################################################
+## CUI
+
+class CUI:
+  """ """
+
+  def __init__(self):
+    ## handle command line arguments
+    self.parser = argparse.ArgumentParser(
+      description=""" CUI """,
+      epilog="\n"
+      )
+    ## declare options
+    self._init_args()
+    ## parse command line args
+    self.args = self.parser.parse_args() ### !!!
+    ## setup logging
+    logging.basicConfig()
+    if self.args.debug:
+      logging.getLogger().setLevel(logging.DEBUG)
+    elif self.args.quiet:
+      logging.getLogger().setLevel(logging.WARNING)
+    else:
+      logging.getLogger().setLevel(logging.INFO)
+
+  def _init_args(self):
+    self.parser.add_argument("param",
+      nargs="*",
+      help="parameters")
+    self.parser.add_argument("--debug",
+      action="store_true",
+      help="Activate debugging output")
+    self.parser.add_argument("--quiet",
+      action="store_true",
+      help="Disable informatic logging")
+
+__all__.append("CUI")
