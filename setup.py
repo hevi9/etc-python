a6b8aadca70397d7208258a59397f22324a80bb5
Petri Heinilä <hevi@lut.fi>
2013-01-24 17:09:15 +0200

    distutils.core => setuptools

--- setup.py
+++ setup.py
@@ -3,11 +3,18 @@
 ## Copyright (C) 2013 Petri Heinilä, License LGPL 2.1
 """ """
 
-from distutils.core import setup
+from setuptools import setup
 import os
 j = os.path.join
 
 scripts = [j("bin",f) for f in os.listdir("bin")]
+
+def isdata(f):
+  if f == "__pycache__": return False
+  if f == ".gitignore": return False
+  return True
+
+datas = [f for f in os.listdir("hevi_proto") if isdata(f)]
 
 setup(
   name='hevi_proto',
@@ -17,5 +24,10 @@ setup(
   url='http://github.com/hevi9/hevi_proto',
   packages=['hevi_proto'],
   scripts = scripts,
-  package_data = {'hevi_proto': ["*"]}
+  package_data = {'hevi_proto': datas},
+  entry_points={
+          'console_scripts':
+              ['release = zest.releaser.release:main',
+               'prerelease = zest.releaser.prerelease:main',
+               ]}
 )
