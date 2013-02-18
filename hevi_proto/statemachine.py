a36e71774006bb0a0146ef23c27e1a44b16f0d92
Petri Heinilä <hevi@lut.fi>
2013-02-18 17:44:38 +0200

    added statemachine idea code

---
+++ hevi_proto/statemachine.py
@@ -0,0 +1,15 @@
+
+
+
+class Sample:
+  
+  states = STATE("INIT","CONN","READY")
+  
+  states = (
+    STATE(      "INIT","CONN","READY")
+    IN("input1","CONN"  )
+  )
+  
+  jj = {
+    input1: ("CONN",)
+  }
