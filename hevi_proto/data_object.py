0eecc31f90eb0f1e96abffc7e1bba29d96828859
Petri Heinilä <hevi@lut.fi>
2013-02-22 18:19:16 +0200

    fixes

--- hevi_proto/data_object.py
+++ hevi_proto/data_object.py
@@ -30,7 +30,7 @@ Iterate all data members usage::
   for d in file.data:
     print("  " + d)
 
-Related:
+Related d1_ d2_ d3_
 
 .. _d1: http://docs.python.org/howto/descriptor.html
 .. _d2: http://wiki.python.org/moin/PythonDecoratorLibrary
