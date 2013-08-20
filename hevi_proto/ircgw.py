4d698b571e591d04eb28a8595eb311eb8dec37ad
hevi <hevi@lut.fi>
2013-08-20 06:23:00 +0300

    ioevents completed

--- hevi_proto/ircgw.py
+++ hevi_proto/ircgw.py
@@ -54,26 +54,41 @@ class Connect(ReRe):
     rere = self
     self = self.ctx
     self.fd = socket.socket()
-    ioevents.read_add(self.fd,self.on_read)
+    ioevents.on_reads[self.fd] = self.on_read
+    ioevents.on_closes[self.fd] = self.on_close
+    ioevents.on_writes[self.fd] = rere.on_write
     self.fd.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    self.fd.setblocking(False)
     self.fo = self.fd.makefile("rw",1)
     D("connecting to {} .. ".format(self.addr))
-    self.fd.connect(self.addr)
+    try:
+      self.fd.connect(self.addr)
+    except BlockingIOError as ex:
+      pass
     D("connected")
-    self.on_readline = rere.on_readline
-    
-  def on_readline(self,line):
+    self.on_reply = rere.on_reply1
+        
+  def on_write(self,fo):
+    D("connected")
+        
+  def on_reply1(self,src,code,dst,rest):
     rere = self
     self = self.ctx
-    D(line)
+    if code != "020":
+      raise Exception("Bad code")
     self.writeline("PASS {}".format(self.token))
     self.writeline("NICK {}".format(self.nick))
     self.writeline("USER {} 0 * :{}".format(self.nick,self.username))
-    self.on_readline = rere.on_readline2
+    self.on_reply = rere.on_reply2
     
-  def on_readline2(self,line):
-    D(line)
-    
+  def on_reply2(self,src,code,dst,rest):
+    rere = self
+    self = self.ctx
+    D("on_reply2({},{})".format(code,rest))
+    if code == 372:
+      log.info(rest)
+    elif code == 376:
+      self.release = True      
 
 ##############################################################################
 class close(ReRe):
@@ -112,15 +127,14 @@ class Proto:
     self.calls.append(Connect(self))
 
   def on_write(self,fd):pass
-  
-  @property
-  def rfd(self):
-    return self.fd
+    
+  def on_close(self,fd):
+    D("on_close({})".format(fd))
   
   def on_read(self,fd):
-    D("on_read({})".format(fd))
+    #D("on_read({})".format(fd))
     data = fd.recv(4096)
-    D("data: {}".format(data))
+    #D("data: {}".format(data))
     self.rbuf = self.rbuf + data.decode()
     pos = self.rbuf.find("\r\n")
     while pos > -1:
@@ -128,9 +142,26 @@ class Proto:
       self.on_readline(line)
       self.rbuf = self.rbuf[pos+2:]
       pos = self.rbuf.find("\r\n")
+    return 1
   
-  def on_readline(self,line): pass # stub
-  
+  def on_readline(self,line): 
+    #D(line)
+    first,rest = line.split(maxsplit=1)
+    if first[0] == ":": # reply ?
+      src = first[1:]
+      code, rest = rest.split(maxsplit=1)
+      dst, rest = rest.split(maxsplit=1)
+      self.on_reply(src, code, dst, rest)
+    else: # cmd
+      cmd = first
+      self.on_cmd(cmd,rest)
+      
+  def on_reply(self,src,code,dst,rest): pass # stub
+
+  def on_cmd(self,cmd,rest):
+    D("cmd: {}, {}".format(cmd,rest))
+    
+
   def writeline(self,line):
     data = (line + "\r\n").encode()
     self.fd.send(data)
@@ -162,27 +193,77 @@ class IOEvents:
   
   def __init__(self):
     self.epoll = select.epoll()
-    self.on_reads = dict() # fileno: (fileobject, on_read)
+    self.allfos = dict() # fo: bits
+    self.on_reads = IOEvents.Event(self,select.EPOLLIN,"IN")
+    self.on_writes = IOEvents.Event(self,select.EPOLLOUT,"OUT") 
+    self.on_closes = IOEvents.Event(self,select.EPOLLHUP,"HUP")
+    self.xxxs = (self.on_reads, self.on_closes)
   
   def dispatch(self,timeout=0):
-    #D("dispatch")
-    #D(self.epoll.poll(timeout))
     for fd, event in self.epoll.poll(timeout):
-      #D("poll: {} {}".format(fd, event))
-      if event & select.EPOLLIN:
-        self.on_reads[fd][1](self.on_reads[fd][0])
-      
-  def read_add(self, fd, on_read):
-    D("read_add({},{})".format(fd,on_read))
-    self.on_reads[fd.fileno()] = (fd,on_read)
-    self.epoll.register(fd,select.EPOLLIN | select.EPOLLHUP)
+      for xxx in self.xxxs:
+        if event & xxx.event:
+          for fo in xxx:
+            if fo.fileno() == fd:
+              more = xxx[fo](fo)
+              if not more or more < 1:
+                del xxx[fo]
+
+  class Event(collections.UserDict):
     
-  def read_remove(self,fd):
-    D("read_remove({},{})".format(fd))
-    self.epoll.unregister(fd)
-    del self.on_reads[fd.fileno()]
+    def __init__(self,ctx,event,name):
+      self.ctx = ctx
+      self.event = event
+      self.name = name
+      self.data = dict()
+
+    def __setitem__(self,fo,func):
+      D("__setitem__({},{})".format(fo,func))
+      self.data[fo] = func
+      bits = self.ctx.allfos.setdefault(fo,0)
+      if bits == 0:
+        self.ctx.epoll.register(fo,self.event)
+      else:
+        self.ctx.epoll.modify(fo,bits | self.event)
+      self.ctx.allfos[fo] = bits | self.event
+      
+    def __delitem__(self,fo):
+      D("__delitem__({})".format(fo))
+      bits = self.ctx.allfos[fo] & ~self.event
+      if bits == 0:
+        self.ctx.epoll.unregister(fo)
+        del self.ctx.allfos[fo]
+      else:
+        self.ctx.epoll.modify(fo,bits)
+      del self.data[fo]
+
       
 ioevents = IOEvents()
+
+
+#   def dispatch(self,timeout=0):
+#     #D("dispatch")
+#     #D(self.epoll.poll(timeout))
+#     for fd, event in self.epoll.poll(timeout):
+#       #D("poll: {} {}".format(fd, event))
+#       if event & select.EPOLLIN:
+#         more = self.on_reads[fd][1](self.on_reads[fd][0])
+#         if more < 1:
+#           self.read_remove(self.on_reads[fd][0])
+#       if event & self.on_hups.event:
+#         for fo in self.on_hups:
+#           if fo.fileno() == fd:
+#             self.on_hups[fo](fo)
+#   def read_add(self, fd, on_read):
+#     D("read_add({},{})".format(fd,on_read))
+#     self.on_reads[fd.fileno()] = (fd,on_read)
+#     self.epoll.register(fd,select.EPOLLIN | select.EPOLLHUP)
+#     
+#   def read_remove(self,fd):
+#     D("read_remove({},{})".format(fd))
+#     self.epoll.unregister(fd)
+#     del self.on_reads[fd.fileno()]
+
   
 ##############################################################################
 def dev():
