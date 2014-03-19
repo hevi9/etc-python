574c93aea130f44502f58245980f4516c4f77ee7
hevi <hevi@lut.fi>
2014-03-19 16:06:59 +0200

    start

---
+++ echo_client.py
@@ -0,0 +1,21 @@
+import asyncio
+
+class EchoClient(asyncio.Protocol):
+    message = 'This is the message. It will be echoed.'
+
+    def connection_made(self, transport):
+        transport.write(self.message.encode())
+        print('data sent: {}'.format(self.message))
+
+    def data_received(self, data):
+        print('data received: {}'.format(data.decode()))
+
+    def connection_lost(self, exc):
+        print('server closed the connection')
+        asyncio.get_event_loop().stop()
+
+loop = asyncio.get_event_loop()
+coro = loop.create_connection(EchoClient, '127.0.0.1', 8888)
+loop.run_until_complete(coro)
+loop.run_forever()
+loop.close()
