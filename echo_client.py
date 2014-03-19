251553fd05370f9b165525c4901d976365c8f183
hevi <hevi@lut.fi>
2014-03-19 22:36:44 +0200

    python library asyncio examples

--- echo_client.py
+++ echo_client.py
@@ -1,21 +1,37 @@
+#!/usr/bin/env python3.4
+## -*- coding: utf-8 -*-
+
+# from http://docs.python.org/3.4/library/asyncio-protocol.html
+
+import logging  # http://docs.python.org/py3k/library/logging.html
+log = logging.getLogger(__name__)
+D = log.debug
 import asyncio
 
 class EchoClient(asyncio.Protocol):
-    message = 'This is the message. It will be echoed.'
+  message = 'This is the message. It will be echoed.'
 
-    def connection_made(self, transport):
-        transport.write(self.message.encode())
-        print('data sent: {}'.format(self.message))
+  def connection_made(self, transport):
+    transport.write(self.message.encode())
+    print('data sent: {}'.format(self.message))
 
-    def data_received(self, data):
-        print('data received: {}'.format(data.decode()))
+  def data_received(self, data):
+    print('data received: {}'.format(data.decode()))
 
-    def connection_lost(self, exc):
-        print('server closed the connection')
-        asyncio.get_event_loop().stop()
+  def connection_lost(self, exc):
+    print('server closed the connection')
+    asyncio.get_event_loop().stop()
 
-loop = asyncio.get_event_loop()
-coro = loop.create_connection(EchoClient, '127.0.0.1', 8888)
-loop.run_until_complete(coro)
-loop.run_forever()
-loop.close()
+
+def main():
+  logging.basicConfig(level=logging.DEBUG)
+  loop = asyncio.get_event_loop()
+  coro = loop.create_connection(EchoClient, '127.0.0.1', 8888)
+  loop.run_until_complete(coro) # run connection
+  loop.run_forever()
+  loop.close()
+  D("done.")
+
+
+if __name__ == "__main__":
+  main()
--- echo_server.py
+++ echo_server.py
@@ -1,27 +1,46 @@
+#!/usr/bin/env python3.4
+## -*- coding: utf-8 -*-
+
+# from http://docs.python.org/3.4/library/asyncio-protocol.html
+
+import logging  # http://docs.python.org/py3k/library/logging.html
+log = logging.getLogger(__name__)
+D = log.debug
 import asyncio
 
+
 class EchoServer(asyncio.Protocol):
-    def connection_made(self, transport):
-        peername = transport.get_extra_info('peername')
-        print('connection from {}'.format(peername))
-        self.transport = transport
+  
+  def connection_made(self, transport):
+    peername = transport.get_extra_info('peername')
+    print('connection from {}'.format(peername))
+    self.transport = transport
 
-    def data_received(self, data):
-        print('data received: {}'.format(data.decode()))
-        self.transport.write(data)
+  def data_received(self, data):
+    print('data received: {}'.format(data.decode()))
+    self.transport.write(data)
+    # close the socket
+    self.transport.close()
 
-        # close the socket
-        self.transport.close()
 
-loop = asyncio.get_event_loop()
-coro = loop.create_server(EchoServer, '127.0.0.1', 8888)
-server = loop.run_until_complete(coro)
-print('serving on {}'.format(server.sockets[0].getsockname()))
 
-try:
+def main():
+  logging.basicConfig(level=logging.DEBUG)
+  loop = asyncio.get_event_loop()
+  coro = loop.create_server(EchoServer, '127.0.0.1', 8888)
+  server = loop.run_until_complete(coro)
+  print('serving on {}'.format(server.sockets[0].getsockname()))  
+  try:
     loop.run_forever()
-except KeyboardInterrupt:
+  except KeyboardInterrupt:
     print("exit")
-finally:
+  finally:
     server.close()
-    loop.close()
+    loop.close()  
+  D("done.")
+
+
+if __name__ == "__main__":
+  main()
+
+
