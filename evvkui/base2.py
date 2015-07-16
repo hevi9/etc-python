2dfa45caaee1ecfe976a74419581f26553909306
hevi <hevi@lut.fi>
2015-07-16 08:05:24 +0300

    phase

---
+++ evvkui/base2.py
@@ -0,0 +1,18 @@
+
+
+class Page:
+
+  def __init__(self, title=None, *subs):
+    pass
+  
+  
+class Text:
+  
+  def __init__(self, *subs):
+    pass
+  
+class Number:
+  
+  def __init__(self):
+    pass
+  
